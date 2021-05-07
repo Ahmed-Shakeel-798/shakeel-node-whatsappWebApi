f2f07fe74d60879570fa4a9e77d7d43f17f29417
Ahmed <ahmed.shakeel.work@gmail.com>
2021-05-07 09:10:09 +0500

    init commit

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ app.post('/sendMessageTextOnly/:id', async (req, res) => {
         });
 
     } catch (error) {
-        console.log(error);
+        console.log(error)
         res.status(500).send({ Error: "Can't send message!" });
     }
 });
